acc8b41a842cf8a26bffef248156283d3d0887f1
joshuahawksworth <joshuahawksworth@me.com>
2024-03-13 15:41:57 +0000

    copying rows

--- src/components/CertificateRow/index.tsx
+++ src/components/CertificateRow/index.tsx
@@ -14,14 +14,46 @@ const CertificateRow: React.FC<CertificateRowProps> = ({
   toggleFavorite,
   isFavorite,
 }) => {
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [copiedMessageVisible, setCopiedMessageVisible] = useState(false);
+
+  const handleCopyToClipboard = () => {
+    navigator.clipboard.writeText(certificate.uniqueNumber);
+    setTooltipVisible(true);
+    setCopiedMessageVisible(true);
+    setTimeout(() => {
+      setTooltipVisible(false);
+      setCopiedMessageVisible(false);
+    }, 2000);
+  };
   const handleFavoriteClick = () => {
     toggleFavorite(certificate);
   };
 
   return (
     <tr key={certificate.id} className="hover:bg-gray-200 border-gray-100 shadow-md ">
-      <td className="pr-16 pl-3 py-3 whitespace-nowrap text-md font-bold text-black">
+            <td 
+        className="pr-16 pl-3 py-3 whitespace-nowrap text-md font-bold text-black"
+        onMouseEnter={() => setTooltipVisible(true)}
+        onMouseLeave={() => setTooltipVisible(false)}
+        onClick={handleCopyToClipboard}
+        style={{ position: 'relative', cursor: 'pointer' }}
+      >
         {certificate.uniqueNumber.length > 20 ? `${certificate.uniqueNumber.slice(0, 25)}...` : certificate.uniqueNumber}
+        {tooltipVisible && (
+          <div 
+            className="absolute bg-black text-white px-2 py-1 rounded border border-gray-400 font-light text-md"
+            style={{ bottom: '-30px', left: '37%', transform: 'translateX(-50%)' }}
+          >
+            <span style={{ position: 'absolute', top: '-7px', left: '50%', transform: 'translateX(-50%)', width: 0, height: 0, borderLeft: '8px solid transparent', borderRight: '8px solid transparent', borderBottom: '8px solid black' }}></span>
+            Click to copy the certificate ID
+          </div>
+        )}
+        {copiedMessageVisible && (
+          <div className="absolute bg-gray-400 text-white px-2 py-1 rounded border border-gray-400 font-light text-md" style={{ top: '7px', left: '-80px', opacity: 1, transition: 'opacity 1s ease-out' }}>
+            Copied
+          </div>
+        )}
       </td>
       <td className="pr-16 pl-2 py-3 whitespace-nowrap text-md font-bold text-black">
         {certificate.companyName}
