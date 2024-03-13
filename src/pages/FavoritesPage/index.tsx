3f417b877288bf68c933b1308e2a80c206455ec5
joshuahawksworth <joshuahawksworth@me.com>
2024-03-13 14:34:38 +0000

    styling fix

--- src/pages/FavoritesPage/index.tsx
+++ src/pages/FavoritesPage/index.tsx
@@ -32,7 +32,7 @@ const FavoritesPage: React.FC = () => {
             Certificates 📄
           </Link>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="flex justify-center">
            <CertificateTable
                 certificates={favorites}
                 toggleFavorite={toggleFavorite}
